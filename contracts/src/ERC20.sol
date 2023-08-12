fd602f32f58c8c7ec9893ade1abbcd98cbbcac9d
123-code <naranjojose256@gmail.com>
2023-08-12 12:17:37 -0500

    contract bug fixes

--- contracts/src/ERC20.sol
+++ contracts/src/ERC20.sol
@@ -1,40 +1,56 @@
 // SPDX-License-Identifier: MIT
 //import "@openzeppelin/contracts/access/Ownable.sol";
-pragma solidity ^ 0.8.18;
 
-contract Governance {
+pragma solidity ^ 0.8.18;
+import"./IERC721.sol";
+
+
+contract Governance{
      uint256 public immutable supply = 200000;
      mapping(address=>uint256)public balanceOf;
+     mapping(uint=>bool)public tokenclaimed;
      bytes32 public name = "Governance";
      bytes32 public Symbol = "GVT";
      uint256 public newsupply;
+     uint256 public claimable;
+     uint256 claimed;
+     uint256 immutable tokenspernft = 10;
+     IMyToken nftcontract;
+ 
 
 
-event MoneyTransfer(address indexed from,address indexed to,uint256 amount);
 event Minted(address indexed to,uint256 amount);
 
-        modifier SupplyNotExceeded(){
-            require(newsupply + 1 <= supply);
-            _;
-        }
 
-     function Transfer(uint256 _amount,address _to)public payable{
-         balanceOf[msg.sender]-=_amount;
-          balanceOf[_to]+=_amount;
-          emit MoneyTransfer(msg.sender, _to, _amount);
-     }
-
-     function Mint(address _to,uint256 amount)public SupplyNotExceeded{
-        
-         balanceOf[_to]+=amount;
-         newsupply+=amount;
-     }
-
-     function GetPayzBalance(address account)public view returns(uint256){
-            return balanceOf[account];
-     }
-//Contract Address:  0x01b4867e43E558456bEeF249059298067a7f6068
+constructor(address _nftcontract){
+    nftcontract = IMyToken(_nftcontract);
 
 }
 
 
+
+     function claimtokens()public{
+         address sender = msg.sender;
+         uint256 balance = nftcontract.balanceOf(sender);
+         uint256 amounttoclaim = 0;
+
+         require(balance > 0,"you dont own any NFTs");
+
+         for(uint i=0;i<balance;i++){
+             require(claimed<=amounttoclaim,"claimed all your nfts");
+             amounttoclaim +=1;
+             claimed+=1;
+             //nftcontract.tokenids[1]=true;
+
+         } 
+         balanceOf[sender] += amounttoclaim*tokenspernft;
+        
+        emit Minted(sender, amounttoclaim);
+     }
+
+
+
+//Contract Address: 0xba70d4aebFeA45201b8916258475142A3065EeCD
+receive() external payable{}
+fallback() external payable{}
+}
